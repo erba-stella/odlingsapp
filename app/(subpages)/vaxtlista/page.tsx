3e39ee07f46edf56c494811481580a9e44907e6d
Marika <liteolika@gmail.com>
2025-04-15 13:32:35 +0200

    added some global styling/layout to the plant-list-page

--- app/(subpages)/vaxtlista/page.tsx
+++ app/(subpages)/vaxtlista/page.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 // import { Suspense } from "react";
 import styles from "@/app/(subpages)/subpages.module.css";
-import s from "@/app/(subpages)/vaxtlista/vaxtlista.module.css";
+// import { AddPlantForm } from "@/app/(subpages)/components/form/addPlantForm";
 import { AddPlantForm } from "@/app/(subpages)/components/form/addPlantForm";
 
 export const metadata: Metadata = {
@@ -12,9 +12,27 @@ export default function VaxtlistaPage() {
 
   return (
     <main className={styles.main}>
-      <section className={s.addPlantSection}>
-        <h2>Lägg till en växt i din växtlista</h2>
-        <AddPlantForm /> 
+      <h2>Min växtlista</h2>
+      <section className={`${styles.vaxtlistaSection}`}>
+        <article>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis a
+            quisquam aperiam saepe earum numquam sunt eos facilis explicabo
+            itaque ut dolor, accusamus libero? Velit aut asperiores praesentium
+            quisquam possimus.
+          </p>
+        </article>
+        <article>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis a
+            quisquam aperiam saepe earum numquam sunt eos facilis explicabo
+            itaque ut dolor, accusamus libero? Velit aut asperiores praesentium
+            quisquam possimus.
+          </p>
+        </article>
+      </section>
+      <section className={`${styles.addPlantSection} ${styles.fullWidth}`}>
+        <AddPlantForm />
         {/* <Suspense fallback={<div>Loading nice data...</div>}>
          
         </Suspense> */}
