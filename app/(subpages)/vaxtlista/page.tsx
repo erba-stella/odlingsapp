34c21ad56c2b144ae8baa5dd7c57a986f959c0df
Marika <liteolika@gmail.com>
2025-04-16 12:01:09 +0200

    Test with plant-type cards on page

--- app/(subpages)/vaxtlista/page.tsx
+++ app/(subpages)/vaxtlista/page.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import styles from "@/app/(subpages)/subpages.module.css";
 // import { AddPlantForm } from "@/app/(subpages)/components/form/addPlantForm";
 import { AddPlantForm } from "@/app/(subpages)/components/form/addPlantForm";
+import { PlantTypeCard } from "@/app/(subpages)/components/plants/plantTypeCard"
 
 export const metadata: Metadata = {
   title: "Min växtlista",
@@ -10,26 +11,39 @@ export const metadata: Metadata = {
 
 export default function VaxtlistaPage() {
 
+  {
+    /* Plant card test data */
+  }
+  const plants = [
+    {
+      created: "2024-06-11",
+      customName: "Super Söt",
+    },
+    {
+      created: "2024-01-01",
+      customName: "Snabbaste lilla gurkan ever",
+    },
+    {
+      created: "2024-08-29",
+      customName: "Giant mastodont",
+    },
+    {
+      created: "2024-02-16",
+      customName: "Gardener's delight",
+    },
+  ];
+
   return (
     <main className={styles.main}>
       <h2>Min växtlista</h2>
       <section className={`${styles.vaxtlistaSection}`}>
-        <article>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis a
-            quisquam aperiam saepe earum numquam sunt eos facilis explicabo
-            itaque ut dolor, accusamus libero? Velit aut asperiores praesentium
-            quisquam possimus.
-          </p>
-        </article>
-        <article>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis a
-            quisquam aperiam saepe earum numquam sunt eos facilis explicabo
-            itaque ut dolor, accusamus libero? Velit aut asperiores praesentium
-            quisquam possimus.
-          </p>
-        </article>
+        {/* Plant card test */}
+          <PlantTypeCard plantType="Tomater" icon="tomato" plants={plants} />
+          <PlantTypeCard plantType="Huvudkål" icon="cabbage" plants={plants} />
+          <PlantTypeCard plantType="Betor" icon="beet" plants={plants} />
+          <PlantTypeCard plantType="Melon" icon="melon" plants={plants} />
+          <PlantTypeCard plantType="Pak-Choi" icon="pak-choi" plants={plants} />
+        
       </section>
       <section className={`${styles.addPlantSection} ${styles.fullWidth}`}>
         <AddPlantForm />
