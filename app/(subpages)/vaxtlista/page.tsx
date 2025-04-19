637a7791aedf4871c9a59e92a4625b26d4817e76
Marika <liteolika@gmail.com>
2025-04-19 20:38:11 +0200

    added a loader, structured the css a bit

--- app/(subpages)/vaxtlista/page.tsx
+++ app/(subpages)/vaxtlista/page.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
-// import { Suspense } from "react";
+import { Suspense } from "react";
 import styles from "@/app/(subpages)/subpages.module.css";
-// import { AddPlantForm } from "@/app/(subpages)/components/form/addPlantForm";
 import { AddPlantForm } from "@/app/(subpages)/components/form/addPlantForm";
 import { PlantList } from "@/app/(subpages)/components/plants/plantList";
 import { ToggleMenuButton } from "@/app/(subpages)/components/ui/buttons/toggleButtons/toggleMenuButton"
@@ -24,9 +23,20 @@ export default function VaxtlistaPage() {
 
   return (
     <main className={styles.main}>
+      <div className={styles.sectionPageIntro}>
       <h2>Min växtlista</h2>
-      <section className={`${styles.vaxtlistaSection}`}>
+        <section>
+          <p>Det här är en kort introduktion till innehållet på sidan.</p>
+        </section>
+      </div>
+
+      <section className={`${styles.sectionPageMainContent}`} id="form-container">
+        <Suspense fallback={<Loader />}>
+          {
+            
+          }
         <PlantList />
+        </Suspense>
       </section>
 
       <section
