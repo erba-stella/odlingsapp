a8b55e52edf09e85cbaf4e7ae7b8a1a30e0fb2aa
Marika <liteolika@gmail.com>
2025-08-06 18:41:50 +0200

    chore: cleanup

---
+++ lib/hooks/localstorage.ts
@@ -0,0 +1,50 @@
+import { useSyncExternalStore, useCallback } from "react";
+
+/*
+    A Local Storage custom hook from Nico Prananta
+    https://www.nico.fyi/blog/ssr-friendly-local-storage-react-custom-hook
+*/
+export const useLocalStorage = (
+  key: string,
+  initialValue?: string | number
+) => {
+  const data = useSyncExternalStore(
+    (onChange) => {
+      const onStorageEvent = (e: Event) => {
+        const customEvent = e as CustomEvent;
+        if (customEvent.detail.key === key) {
+          onChange();
+        }
+      };
+      window.addEventListener("storage", onChange);
+      window.addEventListener(
+        "local-storage-change",
+        onStorageEvent as EventListener
+      );
+      return () => {
+        window.removeEventListener("storage", onChange);
+        window.removeEventListener(
+          "local-storage-change",
+          onStorageEvent as EventListener
+        );
+      };
+    },
+    () => {
+      const data = localStorage.getItem(key);
+      return data || initialValue;
+    },
+    () => initialValue
+  );
+
+  const setData = useCallback(
+    (value: string) => {
+      localStorage.setItem(key, value);
+      window.dispatchEvent(
+        new CustomEvent("local-storage-change", { detail: { key } })
+      );
+    },
+    [key]
+  );
+
+  return [data, setData] as const;
+};
