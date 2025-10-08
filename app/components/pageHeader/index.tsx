1279cbe649a30d5d4616582f101e5246c758895b
Marika <liteolika@gmail.com>
2025-10-09 00:16:41 +0200

    feat(svg-logo): add svg logo-text

--- app/components/pageHeader/index.tsx
+++ app/components/pageHeader/index.tsx
@@ -1,16 +1,16 @@
 import styles from "./pageHeader.module.css";
-import cn from "@/lib/utils/addClassNames"
+import cn from "@/lib/utils/addClassNames";
 import { NavList } from "./NavList";
 import { MobileNavToggleMenu } from "./MobileNavToggleMenu";
+import { LogoText } from "../icons/logoText";
 
-export const PageHeader = ({
- className = "",
-}: {
-  className?: string;
-}) => {
+export const PageHeader = ({ className = "" }: { className?: string }) => {
   return (
     <header id="page-header" className={cn(className, styles.header)}>
-      <h1>SåPlanera</h1>
+      <h1>
+        <span className="visually-hidden">SåPlanera</span>
+        <LogoText aria-hidden="true" className={styles.logotext} />
+      </h1>
 
       {/* Visually hidden link for skip navigation */}
       <a className={styles.skipLink} href="#main-content">
