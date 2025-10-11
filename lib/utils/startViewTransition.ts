3ec8af35a3f1c51ee2039cea324b2d0798d016f5
Marika <liteolika@gmail.com>
2025-10-11 17:36:01 +0200

    refactor: improve viewtransitions

--- lib/utils/startViewTransition.ts
+++ lib/utils/startViewTransition.ts
@@ -1,15 +1,18 @@
 import { flushSync } from "react-dom";
 
-// helper for view transitions
 export const startViewTransition = (func: () => void) => {
   if (!document.startViewTransition) {
-    func();
+    func(); // fallback without transition
     return;
   }
   document.startViewTransition(() => {
-    // use flushSync to make sure DOM updates are done before view transition
-    flushSync(() => {
-      func();
-    });
+    try {
+      // use flushSync to force React to render the DOM update immediately and synchronously.
+      flushSync(func);
+    } catch (err) {
+      console.error("Transition update failed:", err);
+      func(); // fallback without transition
+    }
   });
+    
 };
